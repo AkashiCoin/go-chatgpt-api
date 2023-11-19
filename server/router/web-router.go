24a45ac99ecbcba91affa779aed856e2c76b5480
AkashiCoin <55268546+AkashiCoin@users.noreply.github.com>
2023-11-19 17:05:12 +0800

    feat: support auto freecdn host

--- server/router/web-router.go
+++ server/router/web-router.go
@@ -10,26 +10,26 @@ import (
 	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strings"
 )
 
 func SetWebRouter(router *gin.Engine, buildFS embed.FS, indexPage []byte) {
+	manifestByte, _ := buildFS.ReadFile("web/dist/freecdn-manifest.txt")
+	manifestString := string(manifestByte)
 	router.Use(gzip.Gzip(gzip.DefaultCompression))
 	router.Use(middleware.Cache())
+	router.GET("/freecdn-manifest.txt", FreeCDNManifestHandler(manifestString))
 	router.GET("/auth/login", func(c *gin.Context) {
 		data, _ := buildFS.ReadFile("web/dist/auth/login/index.html")
 		c.Data(http.StatusOK, "text/html; charset=utf-8", data)
 	})
-	router.GET("/", func(c *gin.Context) {
-		if _, err := c.Request.Cookie(common.SESSION_TOKEN_KEY); c.Request.Header.Get(api.AuthorizationHeader) != "" ||
-			err == nil {
-			api.Proxy(c)
-			return
-		}
-		data, _ := buildFS.ReadFile("web/dist/index.html")
-		c.Data(http.StatusOK, "text/html; charset=utf-8", data)
-	})
+	router.GET("/", IndexHandler(indexPage))
 	router.Use(static.Serve("/", utils.EmbedFolder(buildFS, "web/dist")))
-	router.NoRoute(func(c *gin.Context) {
+	router.NoRoute(IndexHandler(indexPage))
+}
+
+func IndexHandler(indexPage []byte) func(c *gin.Context) {
+	return func(c *gin.Context) {
 		if _, err := c.Request.Cookie(common.SESSION_TOKEN_KEY); c.Request.Header.Get(api.AuthorizationHeader) != "" ||
 			err == nil {
 			api.Proxy(c)
@@ -37,5 +37,12 @@ func SetWebRouter(router *gin.Engine, buildFS embed.FS, indexPage []byte) {
 		}
 		c.Header("Cache-Control", "no-cache")
 		c.Data(http.StatusOK, "text/html; charset=utf-8", indexPage)
-	})
+	}
+}
+
+func FreeCDNManifestHandler(manifestString string) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		manifestString = strings.ReplaceAll(manifestString, "localhost", c.Request.Host)
+		c.Data(http.StatusOK, "text/plain; charset=utf-8", []byte(manifestString))
+	}
 }
